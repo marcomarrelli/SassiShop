ec21a9c6a69e283cb187ca347cd17e7fc6c1e26a
marcomarrelli <marcomarrelli03@gmail.com>
2025-01-30 16:20:01 +0100

    feat: Added favicon and Home Page

--- src/template/base.php
+++ src/template/base.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $templateParams["title"]; ?></title>
     <link rel="stylesheet" type="text/css" href="../css/style.css"/>
+    <link rel="icon" href="../assets/images/logo.ico" type="image/x-icon">
 </head>
 
 <body>
--- src/template/home.php
+++ src/template/home.php
@@ -1,1 +1,30 @@
-<!-- qua sarà contentuta la home del sito -->
+<section>
+	<input type="text" placeholder="Cerca prodotto..." />
+	<select>
+		<option value="">Tutte le categorie</option>
+		<option value="1">Sassi Decorati</option>
+		<option value="2">Sassi Personalizzati</option>
+		<option value="3">Sassi da Collezione</option>
+		<option value="4">Kit per Dipingere Sassi</option>
+	</select>
+	<button>Cerca</button>
+	<button style="font-size: smaller;">Filtri</button>
+</section>
+
+<section>
+	<h2>Categorie più amate</h2>
+	<div>
+		<div>Categoria 1</div>
+		<div>Categoria 2</div>
+		<div>Categoria 3</div>
+	</div>
+</section>
+
+<section>
+	<h2>Collezioni più vendute</h2>
+	<div>
+		<div>Collezione 1</div>
+		<div>Collezione 2</div>
+		<div>Collezione 3</div>
+	</div>
+</section>
