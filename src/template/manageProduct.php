91172f815c5304301eb6417d8fb83315470defdf
Margherita <margherita.zanchini@studio.unibo.it>
2025-01-31 18:49:48 +0100

    feat: added page manageProduct for admin

---
+++ src/template/manageProduct.php
@@ -0,0 +1,19 @@
+<!-- <form class="d-none" action="#" method="POST">
+    <ul>
+        <li>
+            <label for="nameProduct">Nome:</label><input type="text" id="nameProduct" name="nameProduct" required/>
+        </li>
+        <li>
+            <label for="category">Scegli una categoria:</label>
+            <select id="cars" name="cars">
+              <option value="volvo">Volvo</option>
+              <option value="saab">Saab</option>
+              <option value="fiat">Fiat</option>
+              <option value="audi">Audi</option>
+            </select>
+            <input type="submit">
+        </li>
+    </ul>
+</form> -->
+
+<input type="button" id="addProduct" value="Aggiungi Prodotto">
