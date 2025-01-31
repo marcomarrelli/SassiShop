05e87e98acacc4fbb43956e2eb563a888843bd40
marcomarrelli <marcomarrelli03@gmail.com>
2025-01-31 14:06:28 +0100

    feat: Added Font and Boostrap Icons

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -6,3 +6,9 @@
     // Connessione al Database
     $dbh = new DatabaseHelper();
 ?>
+
+<!-- Icone Bootstrap -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+
+<!-- Font -->
+<link href='https://fonts.googleapis.com/css?family=Istok Web' rel='stylesheet'>
--- src/template/base.php
+++ src/template/base.php
@@ -5,34 +5,57 @@
 <head>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title><?php echo $templateParams["title"]; ?></title>
-    <link rel="stylesheet" type="text/css" href="../css/style.css"/>
+    <link rel="stylesheet" type="text/css" href="../assets/styles/style.css"/>
     <link rel="icon" href="../assets/images/logo.ico" type="image/x-icon">
 </head>
 
 <body>
     <nav>
         <ul>
-            <li><a href="?page=home"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293zM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5z"/>
-            </svg></a></li>
-            <li><a href="?page=search"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-            </svg></a></li>
-            <li><a href="?page=cart"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
-            <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M3.102 4l1.313 7h8.17l1.313-7zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
-            </svg></a></li>
-            <li><a href="?page=notification"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-bell" viewBox="0 0 16 16">
-            <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
-            </svg></a></li>
-            <li><a href="?page=profile"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
-            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
-            <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
-            </svg></a></li>
+            <li>
+                <a class="header-button" href="?page=home">
+                    <i class="bi bi-house"> </i>
+                </a>
+            </li>
+            <li>
+                <a class="header-button" href="?page=search">
+                    <i class="bi bi-search"> </i>
+                </a>
+            </li>
+            <li>
+                <a class="header-button" href="?page=cart">
+                    <i class="bi bi-cart"> </i>
+                </a>
+            </li>
+            <li>
+                <a class="header-button" href="?page=notification">
+                    <i class="bi bi-bell"> </i>
+                </a>
+            </li>
+            <li>
+                <a class="header-button" href="?page=profile">
+                    <i class="bi bi-person-circle"> </i>
+                </a>
+            </li>
         </ul>
     </nav>
 
     <header>
         <h1>SASSI SHOP</h1>
+        <button id="theme-switcher" onclick="switchTheme()">Change Theme</button>
+        <script>
+            const root = document.documentElement;
+
+            const savedTheme = localStorage.getItem("theme") || "light-mode";
+            root.classList.add(savedTheme);
+
+            function switchTheme() {
+                const newTheme = root.classList.contains("light-mode") ? "dark-mode" : "light-mode";
+
+                root.classList.replace(root.classList.contains("light-mode") ? "light-mode" : "dark-mode", newTheme);
+                localStorage.setItem("theme", newTheme);
+            }
+        </script>
     </header>
 
     <main>
